d0b1b22c6d954129b366a0899db96703f417aa82
maanvikp20 <maanvikpoddar@gmail.com>
2025-10-08 01:07:55 -0700

    e

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const PORT = 5000;
 
@@ -12,5 +13,10 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
   res.sendFile(path.join(__dirname, 'public', 'about.html'))
 })
+
+app.get('/nations', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./data/nations/nations.json'));
+  res.json(data);
+});
 
 app.listen(5000, ()=>{console.log(`Server is running on http://localhost:5000`)})
